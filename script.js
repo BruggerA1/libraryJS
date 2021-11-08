6214ebffb6d5a894b7f9ccf810f862179e222366
Brugger Anderson <BruggerA1@gmail.com>
2021-11-07 20:37:31 -0500

    Backend almost complete

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const bookLibrary = []; 
-const libraryContainer = document.querySelector('main');
+const bookContainer = document.getElementById('bookContainer');
 
 // modal
-const modalWindows = document.getElementById('modal')
-const modalClose = document.getElementById('modal-close');
-modalClose.addEventListener('click', () => modalWindows.classList.add('modal-toggle'))
+const modalForm = document.getElementById('formModal')
+const modalClose = document.getElementById('formClose');
+const formTitle = document.getElementById('formTitle');
+const formAuthor = document.getElementById('formAuthor');
+const formPages = document.getElementById('formPages');
+const formReadBox = document.getElementById('formReadBox');
+modalClose.addEventListener('click', () => modalForm.classList.add('modalToggle'))
 
 // Add Button
-const addButton = document.getElementById('add-button');
-addButton.addEventListener('click', () => modalWindows.classList.remove('modal-toggle'));
+const addButton = document.getElementById('addBookButton');
+addButton.addEventListener('click', () => modalForm.classList.remove('modalToggle'));
 
 class Book {
 	constructor(title, author, pages, read) {
@@ -18,34 +22,54 @@ class Book {
 		this.read = read;
 	}
 	info() {
-		let isRead = (this.read == true) ? 'Already Read' : 'Not Yet Read';
-
-		return `${this.title}, by ${this.author} (${this.pages} pages). ${isRead}.`;
+		return `${this.title} \n ${this.author}`;
 	}
 };
 
 function createBookObject(book) {
 	const bookCard = document.createElement('div');
-	bookCard.classList.add('book-card')
-	const bookInfo = document.createElement('span');
-	bookInfo.innerText = `${book.title} \n ${book.author}`;
-	bookInfo.classList.add('book-info');
-	bookCard.addEventListener('click', () => console.log(book.info()));
+	bookCard.id = "bookCard";
 
-	const bookButton = document.createElement('span');
-	bookButton.innerText = 'clear';
-	bookButton.classList.add('material-icons')
-	bookButton.classList.add('delete-book');
-	bookButton.addEventListener('click', () => deleteBookFromLibrary(book));
+	const itemInfo = document.createElement('span');
+	itemInfo.id = 'bookInfo';
+	itemInfo.innerText = book.info();
+	itemInfo.classList.add('bookInfo')
+	itemInfo.addEventListener('click', () => editBook(book))
 
-	bookCard.appendChild(bookInfo);
-	bookCard.appendChild(bookButton);
-	libraryContainer.prepend(bookCard);
+	const bookStatus = document.createElement('span');
+	bookStatus.innerText = (book.read == true) ? 'READ': 'UNREAD';
+	bookStatus.classList.add('bookStatus');
 
-}
+	const bookDeleteButton = document.createElement('input');
+	bookDeleteButton.type = "button";
+	bookDeleteButton.value = 'clear';
+	bookDeleteButton.classList.add('material-icons');
+	bookDeleteButton.classList.add('button');
+	bookDeleteButton.id = "deleteBook";
+	bookDeleteButton.addEventListener('click', () => deleteBookFromLibrary(book));
+
+	const bookReadToggle = document.createElement('input');
+	bookReadToggle.type = "button";
+	bookReadToggle.value = (book.read == true) ? "check_box" : "check_box_outline_blank";
+	bookReadToggle.classList.add('material-icons');
+	bookReadToggle.classList.add('button');
+	bookReadToggle.id = "toggleBook";
+	bookReadToggle.addEventListener('click', () => toggleReadBook(book));
+
+	const bookStatusContainer = document.createElement('div');
+	bookStatusContainer.classList.add('flex');
+	bookStatusContainer.classList.add('bookStatusContainer');
+
+	bookStatusContainer.appendChild(bookReadToggle);
+	bookStatusContainer.appendChild(bookStatus);
+	bookCard.appendChild(bookStatusContainer)
+	bookCard.appendChild(itemInfo);
+	bookCard.appendChild(bookDeleteButton);
+	bookContainer.prepend(bookCard);
+};
 
 function refreshBookList() {
-	libraryContainer.innerText = null;
+	bookContainer.innerText = null;
 	bookLibrary.forEach(book => createBookObject(book));;
 };
 
@@ -59,17 +83,37 @@ function deleteBookFromLibrary(book) {
 	refreshBookList();
 };
 
+function toggleReadBook(book) {
+	book.read = !book.read;
+	refreshBookList();
+}
+
+function editBook(book) {
+	console.log(book);
+	modalForm.classList.remove('modalToggle');
+
+}
+
+
+
+
+
+
 //init();
-addBookToLibrary(new Book('The Hobbit', 'JRR Tolkien', 294, false));
+addBookToLibrary(new Book('Short Book', 'Author', 294, true));
+addBookToLibrary(new Book('The Very Very Long Book Title of a Very Boring Book', 'Author with Many Names', 294, false));
+
+
 
 refreshBookList();
 
+document.getElementById('formSubmitButton').addEventListener('click', () => submitForm())
 
-document.getElementById('modal-submit').addEventListener('click', () => submitForm())
 
 function submitForm() {
 	const bookForm = document.forms['addBookForm'];
-	const newBook = new Book(bookForm[0].value, bookForm[1].value, bookForm[2].value, bookForm[3].checked)
+	const newBook = new Book(bookForm[1].value, bookForm[2].value, bookForm[3].value, bookForm[4].checked)
 	addBookToLibrary(newBook);
-	modalWindows.classList.toggle('modal-toggle');
+	modalForm.classList.toggle('modalToggle');
 }
+
